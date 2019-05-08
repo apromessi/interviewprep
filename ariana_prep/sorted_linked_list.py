27772643e8a83d762d8cbd24250812f65258d236
Ariana Promessi <ariana@promessi.com>
2019-05-08 13:13:02 -0700

    sorted linked list

--- ariana_prep/sorted_linked_list.py
+++ ariana_prep/sorted_linked_list.py
@@ -83,25 +83,84 @@ class SortedLinkedList(object):
         return current
 
 
-def merge_sort_linked_list(linked_list1, linked_list2):
-    pass
+def split_linked_list(linked_list):
+    current_slow = linked_list.head
+    current_fast = linked_list.head
+
+    while current_fast.next and current_fast.next.next:
+        current_slow = current_slow.next
+        current_fast = current_fast.next.next
+
+    linked_list2 = SortedLinkedList(Node('H'))
+    linked_list2.head.next = current_slow.next
+
+    current_slow.next = None
+
+    return linked_list, linked_list2
+
+
+def merge(linked_list1, linked_list2):
+    current1 = linked_list1.head.next
+    current2 = linked_list2.head.next
+    merged = SortedLinkedList(Node('H'))
+    merged_current = merged.head
+
+    print current1
+    print current2
+
+    while current1 and current2:
+        if current1.value < current2.value:
+            print 'current1 is less'
+            to_add = current1
+            current1 = current1.next
+        else:
+            print 'in else'
+            to_add = current2
+            current2 = current2.next
+
+        print to_add
+        merged.show()
+        merged_current.next = to_add
+        merged_current = merged_current.next
+        to_add.next = None
+
+    if current1:
+        merged_current.next = current1
+    elif current2:
+        merged_current.next = current2
+
+    return merged
 
 
 def main():
-    head = Node('H')
-    linked_list = SortedLinkedList(head)
+    # head = Node('H')
+    # linked_list = SortedLinkedList(head)
 
-    linked_list.insert(5)
-    linked_list.show()
-    linked_list.insert(2)
-    linked_list.show()
-    linked_list.insert(7)
-    linked_list.show()
-    print(linked_list.find(5))
-    print(linked_list.find(1))
-    print(linked_list.find(9))
-    linked_list.remove(1)
-    linked_list.show()
+    # linked_list.insert(5)
+    # linked_list.show()
+    # linked_list.insert(2)
+    # linked_list.show()
+    # linked_list.insert(7)
+    # linked_list.show()
+    # print(linked_list.find(5))
+    # print(linked_list.find(1))
+    # print(linked_list.find(9))
+    # linked_list.remove(1)
+    # linked_list.show()
+
+    elements = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    linked_list1 = SortedLinkedList(Node('H'))
+    for element in elements:
+        linked_list1.insert(element)
+    linked_list1.show()
+
+    linked_list1, linked_list2 = split_linked_list(linked_list1)
+    linked_list1.show()
+    linked_list2.show()
+
+    merged_linked_list = merge(linked_list1, linked_list2)
+    merged_linked_list.show()
 
 if __name__ == '__main__':
     main()
